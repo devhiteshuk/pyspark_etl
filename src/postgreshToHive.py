ade00335a7c0a641afbb77714b967f9d733e824b
Hiteshkumar Dhamshaniya <dev.hitesh.uk@gmail.com>
2024-12-19 16:26:41 +0000

    Change app name

--- src/postgreshToHive.py
+++ src/postgreshToHive.py
@@ -1,7 +1,7 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.master("local").appName("HiteshProject").enableHiveSupport().getOrCreate()
 
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "person").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
