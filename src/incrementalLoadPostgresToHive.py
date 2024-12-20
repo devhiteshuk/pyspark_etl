34a8a87ddc2ba239cd6eb9b1d8d8f492c8a5e46f
Hiteshkumar Dhamshaniya <dev.hitesh.uk@gmail.com>
2024-12-20 10:42:45 +0000

    Make changes in query and logic

--- src/incrementalLoadPostgresToHive.py
+++ src/incrementalLoadPostgresToHive.py
@@ -7,7 +7,7 @@ max_id = spark.sql("SELECT max(id) FROM bigdata_nov_2024.person")
 m_id = max_id.collect()[0][0]
 str(m_id)
 
-query = 'SELECT * FROM bigdata_nov_2024.person' # WHERE id > 206' + str(m_id)
+query = 'SELECT * FROM person WHERE id >' + str(m_id)
 
 more_data = spark.read.format("jdbc") \
     .option("url", "jdbc:postgresql://18.132.73.146:5432/testdb") \
@@ -18,13 +18,13 @@ more_data = spark.read.format("jdbc") \
     .load()
 
 # Added new column of email address 
-more_data_with_email = more_data.withColumn('email_address', F.concat(more_data['name'], F.lit('@gmail.com')))
+#more_data_with_email = more_data.withColumn('email_address', F.concat(more_data['name'], F.lit('@gmail.com')))
 
 
 # Show the updated DataFrame with the new column
-more_data_with_email.show()
-
-more_data_with_email.write.mode("overwrite").saveAsTable("bigdata_nov_2024.person")
+#more_data_with_email.show()
+more_data.show()
+more_data.write.mode("append").saveAsTable("bigdata_nov_2024.person")
 print("Successfully Load to Hive")
 
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/IncreamentalLoadPostgressToHive.py
