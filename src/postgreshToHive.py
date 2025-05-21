f376d30e908308f5be657ba96cdff285da004ae4
Hiteshkumar Dhamshaniya <dev.hitesh.uk@gmail.com>
2025-05-21 10:34:36 +0100

    IP address update

--- src/postgreshToHive.py
+++ src/postgreshToHive.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("HiteshProject").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "person").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.170.23.150:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "person").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
 df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.person")
